eb0e92f78bd9ae44f5dcc16d44ddeca1418ae52c
Mary Kathryn Syms <baglady@Marys-MacBook-Pro.local>
2017-11-17 15:33:06 -0600

    finished Loop Basics Exercise #1 Part a.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,22 @@
+//import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
+        System.out.println("Start with a Basic While Loop");
+//        Scanner scan = new Scanner(System.in);
+
+        int i = 5;
+
+        while (i <= 15) {
+            System.out.print(i + " ");
+            ++i;
+        }
+
+
+
+//        for(int i = 5; i <= 15; i += 1) {
+//            System.out.println();
+//        }
     }
 }
