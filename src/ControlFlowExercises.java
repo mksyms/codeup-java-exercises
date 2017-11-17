9bc495911ec3bf89cd2d290ca7ccf7c204fe20dd
Mary Kathryn Syms <baglady@Marys-MacBook-Pro.local>
2017-11-17 16:44:13 -0600

    finished Loop Basics Exercise #1 Part c. For Loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -6,37 +6,67 @@ public class ControlFlowExercises {
         System.out.println("Start with a Basic Do While Loop");
 //        Scanner scan = new Scanner(System.in);
 
-//--------BASIC LOOP-----------------------------------------//
-//                                                           //
-//        int i = 5;                                         //
-//                                                           //
-//        while (i <= 15) {                                  //
-//            System.out.print(i + " ");                     //
-//            ++i;                                           //
-//        }                                                  //
+//--------WHILE LOOP----------------------------------------//
+//                                                          //
+//        int i = 5;                                        //
+//                                                          //
+//        while (i <= 15) {                                 //
+//            System.out.print(i + " ");                    //
+//            ++i;                                          //
+//        }                                                 //
+//----------------------------------------------------------//
 
 
 
-//--------DO-WHILE LOOP--------------------------------------//
-//      int i = 100;                                         //
-//                                                           //
-//      do {                                                 //
-//          System.out.println(i);                           //
-//          i -= 5;                                          //
-//      } while (i >= -10);                                  //
+//--------DO-WHILE LOOP-------------------------------------//
+//      int i = 100;                                        //
+//                                                          //
+//      do {                                                //
+//          System.out.println(i);                          //
+//          i -= 5;                                         //
+//      } while (i >= -10);                                 //
+//----------------------------------------------------------//
+// double i = 2;                                            //
+//                                                          //
+//        do {                                              //
+//            int castDblToInt = (int) i;                   //
+//            System.out.println(castDblToInt);             //
+//            i = Math.pow(i, 2);                           //
+//        } while (i < 1000000);                            //
+//----------------------------------------------------------//
 
-        double i = 2;
 
-        do {
-            int castDblToInt = (int) i;
-            System.out.println(castDblToInt);
-            i = Math.pow(i, 2);
-        } while (i < 1000000);
-
-
-
+//--------FOR LOOP------------------------------------------//
 //        for(int i = 5; i <= 15; i += 1) {
-//            System.out.println();
+//            System.out.print(i + " ");
 //        }
+//----------------------------------------------------------//
+//        for(int i = 0; i <= 100; i += 2) {
+//            System.out.println(i);
+//        }
+//----------------------------------------------------------//
+//        for(int i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
+//----------------------------------------------------------//
+//        for(double i = 2; i < 1000000; i = Math.pow(i, 2)) {
+//            int castDblToInt = (int) i;
+//            System.out.println(castDblToInt);
+//        }
+//----------------------------------------------------------//
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
