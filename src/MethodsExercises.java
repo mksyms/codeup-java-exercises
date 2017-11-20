2cb08614bf297188493602d6cfc1fc4341238d82
Mary Kathryn Syms <baglady@Marys-MacBook-Pro.local>
2017-11-20 17:03:21 -0600

    Started on Methods Exercise and compiled MethodsExercises.java to create bytecode (.class)

---
+++ src/MethodsExercises.java
@@ -0,0 +1,27 @@
+public class MethodsExercises {
+
+//                                                                                  //
+//----Basic Arithmetic--------------------------------------------------------------//
+//                                                                                  //
+//    Create four separate methods. Each will perform an arithmetic operation:      //
+//                                                                                  //
+//    (+) Addition                                                                  //
+//    (-) Subtraction                                                               //
+//    (*) Multiplication                                                            //
+//    (/) Division                                                                  //
+//                                                                                  //
+//    Each function needs to take two parameters/arguments                          //
+//    so that the operation can be performed.                                       //
+//                                                                                  //
+//    Test your functions and verify the output.                                    //
+//                                                                                  //
+//    Add a modulus function that finds the modulus of two numbers.                 //
+//                                                                                  //
+//    What happens if we try to divide by zero? What should happen?                 //
+//                                                                                  //
+
+
+    public static void main(String[] args) {
+
+    }
+}
